7420c0a7be008d0af3f69cae37af0c5dc968594e
Anuj Shrestha <Anuj.me3@gmail.com>
2018-03-04 22:47:15 +0545

    change little thread page design

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,30 +4,30 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Forum Threads</div>
-
-                    <div class="card-body">
-
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="level">
-                                    <h4 class="flex">
-                                        <a href="{{$thread->path()}}">
-                                            {{$thread->title}}
-                                        </a>
-                                    </h4>
+                @foreach($threads as $thread)
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="level">
+                                <h4 class="flex">
                                     <a href="{{$thread->path()}}">
-                                        <strong>{{$thread->replies_count}} {{str_plural('reply',$thread->replies_count)}}</strong>
+                                        {{$thread->title}}
                                     </a>
-                                </div>
+                                </h4>
+                                <a href="{{$thread->path()}}">
+                                    <strong>{{$thread->replies_count}} {{str_plural('reply',$thread->replies_count)}}</strong>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <article>
+
                                 <div class="body">{{$thread->body}}</div>
 
                             </article>
                             <hr>
-                        @endforeach
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
