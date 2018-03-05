d468a0f94de76fb0870a6f7055d903c6bfa08b30
Anuj Shrestha <Anuj.me3@gmail.com>
2018-03-05 20:04:23 +0545

    fix threadpolicy

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -9,16 +9,16 @@
                     <div class="card-header">
                         <div class="level">
                             <h4 class="flex">
-                                 <a href="{{route('profile',$thread->creator)}}">
-                                {{$thread->creator->name}}
+                                <a href="{{route('profile',$thread->creator)}}">
+                                    {{$thread->creator->name}}
                                 </a> posted:<strong> {{$thread->title}}</strong>
                             </h4>
                             @can('update',$thread)
-                            <form action="{{$thread->path()}}" method="POST">
-                                {{csrf_field()}}
-                                {{method_field('DELETE')}}
-                                <button type="submit" class="btn btn-primary">Delete Thread</button>
-                            </form>
+                                <form action="{{$thread->path()}}" method="POST">
+                                    {{csrf_field()}}
+                                    {{method_field('DELETE')}}
+                                    <button type="submit" class="btn btn-primary">Delete Thread</button>
+                                </form>
                             @endcan
                         </div>
                     </div>
