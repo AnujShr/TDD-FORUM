fd1576b298c188da04253dc0f0e77e883d852108
Anuj Shrestha <Anuj.me3@gmail.com>
2018-03-06 19:30:24 +0545

    fix activitytest

--- tests/Feature/ActivityTest.php
+++ tests/Feature/ActivityTest.php
@@ -40,14 +40,16 @@ class ActivityTest extends TestCase
     public function test_fetches_a_feed_for_any_user()
     {
       $this->signIn();
-      create('App\Thread',['user_id' => auth()->id()]);
-      create('App\Thread',[
-          'user_id' => auth()->id(),
-          'created_at' =>Carbon::now()->subWeek()
-      ]);
+      create('App\Thread',['user_id' => auth()->id()],2);
+
+
+      auth()->user()->activity()->first()->update(['created_at' => Carbon::now()->subWeek()]);
       $feed = Activity::feed(auth()->user());
       $this->assertTrue($feed->keys()->contains(
             Carbon::now()->format('Y-m-d')
+      ));
+      $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
       ));
 
 
