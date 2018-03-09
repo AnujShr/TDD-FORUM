be96ed1d5c4680b909b4ed602da717a52ee51b3a
Anuj Shrestha <Anuj.me3@gmail.com>
2018-03-10 00:53:00 +0545

    add key to reply and moment.js

--- public/js/app.js
+++ public/js/app.js
@@ -48063,6 +48063,7 @@ var render = function() {
       _vm._l(_vm.items, function(reply, index) {
         return _c(
           "div",
+          { key: reply.id },
           [
             _c("reply", {
               attrs: { data: reply },
